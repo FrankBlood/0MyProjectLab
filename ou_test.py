839f3e63e80cab27746d05559d073f5227154874
FrankBlood <1062947208@qq.com>
2016-06-26 15:56:49 +0800

    transfer data as test

---
+++ ou_test.py
@@ -0,0 +1,38 @@
+import os
+import random
+
+filelist = os.listdir('./ImageCLEFTestSetGROUNDTRUTH')
+images = {}
+name = {}
+
+print filelist
+
+for files in filelist:
+    if files.endswith('.txt'):
+        continue
+    if files == '.directory':
+        continue
+    images[filelist.index(files)] = []
+    name[filelist.index(files)] = files
+    imagefile = os.listdir('./ImageCLEFTestSetGROUNDTRUTH/'+files)
+    if imagefile == None:
+        continue
+    for image in imagefile:
+        if image.endswith('.jpg'):
+            images[filelist.index(files)].append('./'+files+'/'+image)
+
+name_data = open('test_name_data.txt','w')
+train_data = open('test_data.txt','w')
+
+for (ID, imagefile) in images.items():
+    try:
+        for image in imagefile:
+            train_data.write(str(image)+'\t'+str(ID)+'\n')
+    except:
+        print ID, images[ID]
+train_data.close()
+
+for (ID, files) in name.items():
+    name_data.write(str(files)+'\t'+str(ID)+'\n')
+name_data.close()
+
